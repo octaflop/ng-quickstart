c74483d88c18289e39f12bf4496ce63e5cb5693a
Faris Chebib <faris@zaneprep.com>
2016-05-05 13:34:17 -0600

    example of separated template

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,19 +3,27 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
 
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @RouteConfig([
   {
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
-  }
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
 ])
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
     <a [routerLink]="['Heroes']">Heroes</a>
     <router-outlet></router-outlet>
   `,
